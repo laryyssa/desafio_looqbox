cf932040ac1beb2ab8d4673b0508fb12e2cb59ac
Laryssa Paiva <laryssapaiva@id.uff.br>
2026-04-04 21:02:28 -0300

    add: question 3 sql

---
+++ sql_test/queries/question_3.sql
@@ -0,0 +1,9 @@
+select 
+	sc.BUSINESS_NAME,
+	SUM(ss.SALES_VALUE)
+from data_store_cad sc
+inner join data_store_sales ss on sc.STORE_CODE = ss.STORE_CODE 
+WHERE 
+	ss.DATE >= "2019-01-01" AND 
+	ss.DATE < "2019-05-01"
+group by sc.BUSINESS_NAME 
